b7babf38e565413fe7f5e57e151f8f7783fe86c2
sumit735 <sumitkumardash1999@gmail.com>
2020-07-10 19:36:50 +0530

    complete

--- index.js
+++ index.js
@@ -1,49 +1,22 @@
-var pdfFiller   = require('pdffiller');
-var express = require('express');
+const express = require('express');
 const pdftk = require('node-pdftk');
 
-var app = express();
+const app = express();
+const bodyParser = require('body-parser')
 
-var sourcePDF = "pan.pdf";
-var destinationPDF =  "pann.pdf";
-var data = {
-    'AO_Code' : '555' +' ' + 'ww'+' ' + '6'+' ' + '6',
-    'last_name' : 'Dash',
-    'first_name' : 'Sumit',
-    'middle_name' : 'Kumar',
-    'day' : '13',
-    'month' : '01',
-    'year' : '1999',
-    'abbreviation1' : 'Sumit' + " " + 'kum'+ " " + 'Dash',
-    'genderData' : 'male',
-    'flname' : 'Dash',
-    'ffname' : 'Subrat',
-    'fmname' : 'Kumar',
-    'afc' : 'kkkk',
-    'village' : 'suhmro',
-    'po' : 'Andsddnsdd',
-    'locality' : 'dfhdfhjdk',
-    'district' : 'ghdgfhfd',
-    'pin' : '751015',
-    'state' : 'Odisha',
-    'country' : 'India',
-    'mobile' : '+91         '+'9853174100',
-    'email' : 'kumardash1@gmail.com',
-    'aadharnumber' : 'djhdvfbhnkfdhbf',
-    'nameAsAadhar' : 'cdjhgdhjfkdlf',
-    'idProof' : 'cgdhjvbhc',
-    'addProof' : 'dkjbhjdvdv',
-    'dobProof' : 'dfhuygdfhdf',
-    'signName' : 'sljfhgvhbdj',
-    'place' : 'dhufgdhfjd',
-    'Date' : '22/10/2020',
-    'self' : 'self'
-
-};
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', async (req, res) => {
+    
+    res.send('Unauthorized Access.')
+    
+});
+
+app.post('/generatePdf', (req, res) => {
+    const data = JSON.parse(req.body.data);
+    console.log(data);
     pdftk
     .input('./pan.pdf')
     .fillForm(data)
@@ -53,10 +26,9 @@ app.get('/', async (req, res) => {
         res.send(buf);
     })
     .catch(err => {
-        // handle errors
+        
     });
-    
-})
+});
 
 var port = process.env.PORT || 3000
 
